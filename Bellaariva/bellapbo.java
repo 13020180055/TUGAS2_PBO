9d8ac68bbf7e89a0dc3e4976788105e45006d3fe
13020180055 <61979075+13020180055@users.noreply.github.com>
2020-03-16 08:21:07 -0700

    Add files via upload

---
+++ Bellaariva/bellapbo.java
@@ -0,0 +1,18 @@
+/*   Nama         :Bella Ariva
+     Kelas        :B1
+     Waktu        :21.59
+     Hari/Tanggal :senin/16 maret 2020
+*/
+public class bellapbo{
+      public static void main(String[] args){
+      String Nim           ="13020180055";
+      String Nama          ="Bella Ariva";
+      String Jurusan       ="Teknik Informatika";
+      String Fakultas      ="Ilmu Komputer";
+      System.out.println("Nim saya : "+ Nim);
+      System.out.println("Nama saya : "+ Nama);
+      System.out.println("Jurusan saya : "+ Jurusan);
+      System.out.println("Fakultas saya :"+ Fakultas);
+}
+}
+     
